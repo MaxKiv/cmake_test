bb1a130e9afc0ee46663a01985e95f9e2a02489c
Max Kivits <maxkivits42@gmail.com>
2022-04-21 21:18:00 +0200

    started lib

---
+++ lib/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+#include <numeric>
+
+template <typename T>
+void printVector (std::vector<T>& vector) {
+    for(const auto& el: vector) {
+        std::cout << el << "";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::cout << "first some bullshit" << std::endl;
+
+    std::map<int, int> mappie{{1,100}, {2,200}, {3,300}};
+    std::string init = "";
+    std::string out = std::accumulate(std::begin(mappie), std::end(mappie), init, [](std::string out, std::map<int, int>::value_type& p) { return out + std::to_string(p.second); });
+    std::cout << std::stoi(out)*100 << std::endl;
+
+    std::cout << "Then some more bullshit" << std::endl;
+
+    std::map<unsigned short, std::string> Map2 {{1,"A"},{2,"B"},{3,"C"},{4,"D"},{5,"E"},{6,"F"}};
+
+    std::string out2 = std::accumulate(std::begin(Map2), std::end(Map2), init, [](std::string out, std::map<unsigned short, std::string>::value_type& entry){return out + std::to_string(entry.first) + " " + entry.second + " ";});
+    std::cout << out
+
+    std::vector<std::string> inputVector("a","b","c");
+    std::cout << "POWER set:\n";
+    printVector(powerset(inputVector));
+
+    return 0;
+}
