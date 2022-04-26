741193b0a5e82019d68e966f7d004effbec2ef75
Max Kivits <maxkivits42@gmail.com>
2022-04-26 17:02:16 +0200

    quick

--- lib/libpower/libpower.hpp
+++ lib/libpower/libpower.hpp
@@ -1,26 +1,21 @@
 #include <vector>
 #include <cmath>
-
+// calculate power set of input vector
 template <typename T>
 std::vector<T> powerSet(std::vector<T>& inputVector) {
-    //
+    // str[000]
+    // str[001]
+    // str[010]
+    // str[011]
     std::vector<T> out;
     for (size_t i = 0; i < std::pow(2,inputVector.size()); i++) {
         {
-            if(i|0b000) {
-                out.push_back(inputVector[0b000]);
+            for(size_t bitIdx = 1; bitIdx < inputVector.size(); bitIdx++) {
+                auto out = i<<bitIdx;
+                std::cout << " i << bitIdx " << out << std::endl;
             }
-            else if(i|0b001){
-                out.push_back(out[0b001]);
-            }
-            else{
-
-            }
+            std::cout << std::endl;
         }
-        // str[000]
-        // str[001]
-        // str[010]
-        // str[011]
     }
     return out;
 }
